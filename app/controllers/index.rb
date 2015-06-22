fe9fd95641a3593904be8840f933290a42e800c0
Steven G. Harms <steven@devbootcamp.com>
2015-06-22 17:03:22 -0400

    Move injected link to be retreived by AJAX

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,7 @@
 get '/' do
   erb :index
 end
+
+get '/derp-link' do
+  "<li><a class='derp-link' href='http://iluvisis.com'>derp-element</a></li>"
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
   $("#insert-li").on('click', function(event) {
-    $("#list").append("<li><a class='derp-link' href='http://iluvisis.com'>derp-element</a></li>");
+    $.ajax('/derp-link')
+      .then(function(data) {
+        $("#list").append(data);
+      })
+      .fail(function() {
+        console.log("Leggo my eggo");
+      });
   });
 
   $("ul").on('click', function(event) {
