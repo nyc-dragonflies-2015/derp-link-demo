6ec5817c3b30241a842f02b75f5775103bcaefd1
Steven G. Harms <steven@devbootcamp.com>
2015-06-22 17:19:56 -0400

    BizarroSheldon thinks good is bad
    Bizzarros think good is bad and bad is good.  So
    he thinks that this is good code.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,6 +3,5 @@ get '/' do
 end
 
 get '/derp-link' do
-  locals =  { url: rand > 0.5 ? 'http://devbootcamp.com' : 'http://stevengharms.com' }
-  erb  :'_derp-link', layout: false, locals: locals
+  erb  :'_derp-link', layout: false
 end
